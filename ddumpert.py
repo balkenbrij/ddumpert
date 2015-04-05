3e0b82526d0e76384807c868889faa074d3e822e
balkenbrij <no@spam>
2015-04-05 21:19:13 +0200

    use python3 instead of 2 and clean up a bit

--- ddumpert.py
+++ ddumpert.py
@@ -1,8 +1,6 @@
-#! /usr/bin/python
-import urllib2
-import urllib
+#! /usr/bin/python3
+import urllib.request, urllib.parse, urllib.error
 import base64
-import sys
 import ast
 import sys
 import os
@@ -11,29 +9,29 @@ import re
 DUMPERT_RE = re.compile('data-files="(.+?)"')
 USERAGENT = {'User-Agent': 'Mozilla/5.0'}
 
-class MyUrlOpener(urllib.FancyURLopener):
+class MyUrlOpener(urllib.request.FancyURLopener):
     version = USERAGENT['User-Agent']
     
 def geturl(url):
-    request = urllib2.Request(url, None, USERAGENT)
-    return urllib2.urlopen(request).read()
+    request = urllib.request.Request(url, None, USERAGENT)
+    return urllib.request.urlopen(request).read()
 
 def getmovieurl(html):
     match = re.search(DUMPERT_RE, html)
     if match:
-        dict = ast.literal_eval(base64.b64decode(match.group(1)))
-        try:    return dict['720p'].replace('\\', '')
-        except: return dict['flv'].replace('\\', '')
+        try:
+            formats = ast.literal_eval(base64.b64decode(match.group(1)))
+        except:
+            return None
+        try:    return formats['720p'].replace('\\', '')
+        except: return formats['flv'].replace('\\', '')
     else:
         return None
 
 def reporthook(numblocks, blocksize, filesize, name=None):
-    try:
-        percent = min((numblocks*blocksize*100)//filesize, 100)
-    except:
-        percent = 100
-    if numblocks != 0:
-        sys.stdout.write("\r")
+    try:    percent = min((numblocks*blocksize*100)//filesize, 100)
+    except: percent = 100
+    if numblocks != 0: sys.stdout.write("\r")
     sys.stdout.write("{0:<66}{1:3d}%".format(name, percent))
 
 if len(sys.argv) != 2:
@@ -43,15 +41,14 @@ if len(sys.argv) != 2:
 movieurl = getmovieurl(geturl(sys.argv[1]))
 if movieurl:
     filename = os.path.basename(movieurl)
-    print("Downloading '" + movieurl + "'..")
-    urllib._urlopener = MyUrlOpener()
+    print(("Downloading '" + movieurl + "'.."))
+    urllib.request._urlopener = MyUrlOpener()
     if sys.stdout.isatty():
-        urllib.urlretrieve(movieurl, filename,
+        urllib.request.urlretrieve(movieurl, filename,
                            lambda nb, bs, fs, name=filename: \
                            reporthook(nb, bs, fs, filename))
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
     else:
-        urllib.urlretrieve(movieurl, filename)
+        urllib.request.urlretrieve(movieurl, filename)
 else:
     print("no video data found in page")
-    
